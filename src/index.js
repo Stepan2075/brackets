feaae04c1ef5f07b3309a46d061e4afc3909cd6b
Stepan <lavash@tut.by>
2021-03-16 00:41:27 +0300

    feat: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let chars = str.split(''),
+        stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        closeIndex,
+        openIndex;
+    for (let i = 0; i < chars.length; i++) {
+       openIndex = open.indexOf(chars[i]);
+       if (openIndex !== -1) {
+           stack.push(openIndex);
+           continue;
+       }
+       closeIndex = close.indexOf(chars[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
 }
